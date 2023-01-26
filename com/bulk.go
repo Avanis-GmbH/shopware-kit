7539b6f0a7cdcaa547ed65160324d2172b870e30
Valentin Kaiser <valentin@kaiser-bielefeld.de>
2023-01-26 16:02:59 +0100

    [ADD] upsert and delete

--- com/bulk.go
+++ com/bulk.go
@@ -2,6 +2,16 @@ package com
 
 import "net/http"
 
+type SyncOperation struct {
+	Entity  string      `json:"entity"`
+	Action  string      `json:"action"`
+	Payload interface{} `json:"payload"`
+}
+
+type DeleteEntity struct {
+	Id string `json:"id"`
+}
+
 func (c *Client) Sync(ctx ApiContext, payload map[string]SyncOperation) (*http.Response, error) {
 	req, err := c.NewRequest(ctx, http.MethodPost, "/api/_action/sync", payload)
 
@@ -12,8 +22,24 @@ func (c *Client) Sync(ctx ApiContext, payload map[string]SyncOperation) (*http.R
 	return c.Do(ctx.Context, req, nil)
 }
 
-type SyncOperation struct {
-	Entity  string      `json:"entity"`
-	Action  string      `json:"action"`
-	Payload interface{} `json:"payload"`
+func (c *Client) Upsert(ctx ApiContext, entity []*interface{}) (*http.Response, error) {
+	return c.Sync(ctx, map[string]SyncOperation{c.getSegment(entity): {
+		Entity:  c.getSegment(entity),
+		Action:  "upsert",
+		Payload: entity,
+	}})
+}
+
+func (c *Client) Delete(ctx ApiContext, entity interface{}, ids []string) (*http.Response, error) {
+	payload := make([]DeleteEntity, 0)
+
+	for _, id := range ids {
+		payload = append(payload, DeleteEntity{Id: id})
+	}
+
+	return c.Sync(ctx, map[string]SyncOperation{c.getSegment(entity): {
+		Entity:  c.getSegment(entity),
+		Action:  "delete",
+		Payload: payload,
+	}})
 }
