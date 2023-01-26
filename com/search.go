4f803998a65717161ddb10024b550136e703f51e
Valentin Kaiser <valentin@kaiser-bielefeld.de>
2023-01-26 23:47:21 +0100

    [FIX] Collection method scopes and errors

--- com/search.go
+++ com/search.go
@@ -3,29 +3,29 @@ package com
 import (
 	"net/url"
 
-	"github.com/Avanis-GmbH/SUC/model"
+	"github.com/pkg/errors"
 )
 
-func (c *Client) Search(ctx ApiContext, criteria Criteria, v model.Collection) error {
+func (c *Client) Search(ctx ApiContext, criteria Criteria, v Collection) error {
 	url, err := url.JoinPath("/api/search", c.getSegment(v))
 	if err != nil {
-		return err
+		return errors.Wrapf(err, "failed to join path for %T", v)
 	}
 
 	req, err := c.NewRequest(ctx, "POST", url, criteria)
 	if err != nil {
-		return err
+		return errors.Wrapf(err, "failed to create request for %T", v)
 	}
 
 	_, err = c.Do(ctx.Context, req, v)
 	if err != nil {
-		return err
+		return errors.Wrapf(err, "failed to do Search request for %T", v)
 	}
 
 	return nil
 }
 
-func (c *Client) SearchAll(ctx ApiContext, criteria Criteria, v model.Collection) error {
+func (c *Client) SearchAll(ctx ApiContext, criteria Criteria, v Collection) error {
 	if criteria.Limit == 0 {
 		criteria.Limit = 50
 	}
@@ -40,36 +40,36 @@ func (c *Client) SearchAll(ctx ApiContext, criteria Criteria, v model.Collection
 
 		err := c.Search(ctx, criteria, v)
 		if err != nil {
-			return err
+			return errors.Wrapf(err, "failed to search for %T", v)
 		}
 
-		if len(v.GetData()) == 0 {
+		if len(v.getData()) == 0 {
 			break
 		}
 
-		result = append(result, v.GetData()...)
+		result = append(result, v.getData()...)
 	}
 
-	v.SetTotal(int64(len(result)))
-	v.SetData(result)
+	v.setTotal(int64(len(result)))
+	v.setData(result)
 	return nil
 }
 
 func (c *Client) SearchIds(ctx ApiContext, criteria Criteria, v interface{}) (*SearchIdsResponse, error) {
 	url, err := url.JoinPath("/api/search-ids", c.getSegment(v))
 	if err != nil {
-		return nil, err
+		return nil, errors.Wrapf(err, "failed to join path for %T", v)
 	}
 
 	req, err := c.NewRequest(ctx, "POST", url, criteria)
 	if err != nil {
-		return nil, err
+		return nil, errors.Wrapf(err, "failed to create request for %T", v)
 	}
 
 	data := &SearchIdsResponse{}
 	_, err = c.Do(ctx.Context, req, data)
 	if err != nil {
-		return data, err
+		return data, errors.Wrapf(err, "failed to do searchIds request for %T", v)
 	}
 
 	return data, nil
