ebe5767d0ab7fbbdd9c2ebf41cb5876d3b4d543c
Valentin Kaiser <valentin@kaiser-bielefeld.de>
2023-01-27 15:50:48 +0100

    [FIX] SystemConfig model

--- model/system_config.go
+++ model/system_config.go
@@ -3,15 +3,11 @@ package model
 import "time"
 
 type SystemConfig struct {
-	ConfigurationKey   string                   `json:"configurationKey"`   // required
-	ConfigurationValue SystemConfigurationValue `json:"configurationValue"` // required
-	CreatedAt          time.Time                `json:"createdAt,omitempty"`
-	Id                 string                   `json:"id,omitempty"`
-	SalesChannel       *SalesChannel            `json:"salesChannel,omitempty"`
-	SalesChannelId     string                   `json:"salesChannelId,omitempty"`
-	UpdatedAt          time.Time                `json:"updatedAt,omitempty"`
-}
-
-type SystemConfigurationValue struct {
-	Value interface{} `json:"_value,omitempty"`
+	ConfigurationKey   string        `json:"configurationKey"`   // required
+	ConfigurationValue interface{}   `json:"configurationValue"` // required
+	CreatedAt          time.Time     `json:"createdAt,omitempty"`
+	Id                 string        `json:"id,omitempty"`
+	SalesChannel       *SalesChannel `json:"salesChannel,omitempty"`
+	SalesChannelId     string        `json:"salesChannelId,omitempty"`
+	UpdatedAt          time.Time     `json:"updatedAt,omitempty"`
 }
